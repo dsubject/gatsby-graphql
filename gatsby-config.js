e76cf175df4bf3ecb04cd9b10838959dd87ee997
Danielle Subject <daniellesubject@MBPe15C02RWM5MFVH3.local>
2018-05-24 22:56:15 -0400

    querying site data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,16 @@
 module.exports = {
     // tossing in a dynamic title
         siteMetadata: {
-          title: `Blah Blah Fake Title`,
+          title: `Dani's Fake Title`,
         },
     plugins: [
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+              name: `src`,
+              path: `${__dirname}/src/`,
+            },
+          },
       `gatsby-plugin-glamor`,
       {
         resolve: `gatsby-plugin-typography`,
