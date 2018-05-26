ee5da3f821c24d3375e5669ead0e1f07e1874d10
Danielle Subject <daniellesubject@MBPe15C02RWM5MFVH3.local>
2018-05-26 19:09:15 -0400

    creating slugs with plugin

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,10 +9,23 @@
 
 
 // new function that now only looks at MarkdownRemark nodes
-exports.onCreateNode = ({ node, getNode }) => {
+/* exports.onCreateNode = ({ node, getNode }) => {
   if (node.internal.type === `MarkdownRemark`) {
     // traversing the "node graph" to its parent File node
     const fileNode = getNode(node.parent)
     console.log(`\n`, fileNode.relativePath)
+  }
+}; */
+
+
+// now using gatsby-source-filesystem plugin to create slugs
+
+// this function handles finding the parent File node along with
+//  creating the slug
+const { createFilePath } = require(`gatsby-source-filesystem`);
+
+exports.onCreateNode = ({ node, getNode }) => {
+  if (node.internal.type === `MarkdownRemark`) {
+    console.log(createFilePath({ node, getNode, basePath: `pages` }))
   }
 };
